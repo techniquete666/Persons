9432a325860ce7eb7970571db084185112fd6358
techniquete666 <dodyaozxc@gmail.com>
2022-10-19 17:40:24 +0300

    Initial Commit

---
+++ Persons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Persons
+//
+//  Created by Vyacheslav on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
